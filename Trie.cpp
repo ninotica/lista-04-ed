a31892c01f75e090e2b71dd299b4d2e75421d4c7
Trajano <pedroetrajano@gmail.com>
2026-06-06 15:02:51 -0300

    trie

---
+++ Trie.cpp
@@ -0,0 +1,26 @@
+#include "Trie.hpp"
+
+Trie::Trie() {
+
+}
+Trie::~Trie() {
+
+}
+
+bool Trie::insert(Game* game) {
+
+}
+bool Trie::contains(std::string title) {
+
+}
+
+std::vector<Game*> Trie::autocomplete(std::string prefix, int k){
+
+}
+
+std::string Trie::toSearchKey(std::string text) {
+
+}
+void Trie::sortResults(std::vector<Game*>& games) {
+
+}
